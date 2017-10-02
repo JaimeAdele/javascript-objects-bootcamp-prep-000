5c549bded61762a630a883912941a685bc5ee8ff
Jaime Zuspann <jaimeadele@gmail.com>
2017-10-02 06:30:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.append(playlist, {artistName: songTitle});
+  Object.assign(playlist, {artistName: songTitle});
 }
